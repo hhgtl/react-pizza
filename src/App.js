241d878ccf22e3edf90fed3b8e51f74faa5d0919
Petro <petrosahal66@gmail.com>
2024-06-08 14:11:32 +0300

    feat: deleted pizzas.json in assets folder now use this mockapi to get pizzas.json

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import Header from './components/Header/Header';
 import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 import Sort from './components/Sort/Sort';
 import './scss/app.scss';
-import pizzas from './assets/pizzes.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pizzasData, setPizzasData] = useState([]);
+  useEffect(() => {
+    fetch('https://666438f6932baf9032aa655c.mockapi.io/react-pizza')
+      .then((res) => res.json())
+      .then((data) => {
+        setPizzasData(data);
+      });
+  }, []);
+  console.log(pizzasData);
   return (
     <div className="App">
       <div className="wrapper">
@@ -18,7 +27,7 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzas.map((item) => (
+              {pizzasData.map((item) => (
                 <PizzaBlock key={item.id} {...item} />
               ))}
             </div>
