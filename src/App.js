cf9a7ee73ea25d7714a63c9fefacf81af64f396f
Petro <petrosahal66@gmail.com>
2024-06-08 17:55:39 +0300

    feat: add skeleton before loading pizzablock

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import Categories from './components/Categories/Categories';
 import Header from './components/Header/Header';
 import PizzaBlock from './components/PizzaBlock/PizzaBlock';
+import PizzaBlockSkeleton from './components/PizzaBlock/PizzaBlockSkeleton';
 import Sort from './components/Sort/Sort';
 import './scss/app.scss';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [pizzasData, setPizzasData] = useState([]);
+  const [isLoadingPizza, setIsLoadingPizza] = useState(true);
   useEffect(() => {
     fetch('https://666438f6932baf9032aa655c.mockapi.io/react-pizza')
       .then((res) => res.json())
       .then((data) => {
         setPizzasData(data);
+        setIsLoadingPizza(false);
       });
   }, []);
-  console.log(pizzasData);
   return (
     <div className="App">
       <div className="wrapper">
@@ -27,9 +29,9 @@ function App() {
             </div>
             <h2 className="content__title">Все пиццы</h2>
             <div className="content__items">
-              {pizzasData.map((item) => (
-                <PizzaBlock key={item.id} {...item} />
-              ))}
+              {isLoadingPizza
+                ? [...new Array(6)].map((_, i) => <PizzaBlockSkeleton key={i} />)
+                : pizzasData.map((item) => <PizzaBlock key={item.id} {...item} />)}
             </div>
           </div>
         </div>
